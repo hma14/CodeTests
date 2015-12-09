451f5451da238d96a947d87b7006e4d571ea17d4
Henry <henryma14@gmail.com>
2015-12-09 12:49:50 -0800

    Added CaesarCipher

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class CaesarCipher
+    {
+        static string Caesar(string str, int k)
+        {
+            var ret = new string(str.ToCharArray().Select(c => Convert.ToChar(c + k)).ToArray());
+            return ret;
+        }
+        static void Main(string[] args)
+        {
+            string input = "Variable value will now have 97 which corresponds to the value of that ascii character";
+            Console.WriteLine(Caesar(input, 3));
+        }
+    }
+}
